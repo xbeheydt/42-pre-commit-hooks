dd6a08721890857b3d2f1591b8ae686ff137dfa9
xavier.beheydt@gmail.com <xavier.beheydt@gmail.com>
2021-11-04 11:17:06 +0100

    fixing files includes

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
         name='pre_commit_dummy_package',
-        version='0.2.0',
+        version='0.2.1',
         install_requires=['norminette==3.3.40'],
 )
