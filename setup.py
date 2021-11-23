d390268c9af42519e45f890b3eb8c9d990e0ba40
xbeheydt <xavier.beheydt@gmail.com>
2021-11-23 10:14:11 +0100

    update to common unitests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='pre_commit_dummy_package',
-        version='0.2.3',
+        version='0.2.4',
         install_requires=[
             'norminette==3.3.40',
             'pre-commit=2.15.0'
